251bd89765ac8f6d47dbd589d8864714effbcae3
jayrungta <Jay@Jays-MBP.fios-router.home>
2019-10-02 18:48:37 -0400

    initalize

---
+++ gatsby-starter-solidstate/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: 'Jay Rungta', // <title>
+  manifestName: 'JayRungta',
+  manifestShortName: 'JayRungta', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Jay Rungta',
+  subHeading: 'Software Development with Design Thinking',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/jayrungta',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/jayrungta',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:rungta.jay@gmail.com',
+    },
+  ],
+  phone: '',
+  address: 'New York, NY',
+};
