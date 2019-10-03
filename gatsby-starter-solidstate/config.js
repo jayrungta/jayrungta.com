985b0a519b8681705b53195d3ff880c4289d8700
jayrungta <Jay@Jays-MBP.fios-router.home>
2019-10-02 20:48:07 -0400

    fix prefix path

--- gatsby-starter-solidstate/config.js
+++ gatsby-starter-solidstate/config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/jayrungta.com`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Jay Rungta',
   subHeading: 'Software Development with Design Thinking',
   // social
